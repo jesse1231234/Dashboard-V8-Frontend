8cc7ab05b6a864df96611a251ec1ecbd185ac3d2
jesse1231234 <jesse.thompson@colostate.edu>
2026-01-07 09:06:28 -0700

    Update tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,27 @@
-/** @type {import('tailwindcss').Config} */
-const config = {
+// tailwind.config.mjs
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      borderRadius: {
+        xl: "0.9rem",
+        "2xl": "1.1rem",
+      },
+      ringColor: {
+        DEFAULT: "#1E4D2B",
+      },
       colors: {
         csuGreen: "#1E4D2B",
-        csuGold: "#C8C372"
-      }
-    }
+        csuGold: "#C8C372",
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
 
 export default config;
